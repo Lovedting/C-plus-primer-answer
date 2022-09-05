18b1b43eefe364e27e8da66f8af30e7ebd2eaaec
Lovedting <110101755+Lovedting@users.noreply.github.com>
2022-09-05 22:34:31 +0800

    Add files via upload

---
+++ 4_6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+struct CandyBar{
+    std::string brand;
+    double weight;
+    int calorie;      
+};
+
+int main(void)
+{
+    using namespace std;
+    
+    CandyBar snacks[3] = {
+        {"Mocha Munch", 2.3, 350},
+        {"Mocha Munch", 2.3, 350},
+        {"Mocha Munch", 2.3, 350}
+    };
+    
+    for(int i = 0; i < 3; ++i)
+    {
+        cout << "Brand: " << snacks[i].brand << endl;
+        cout << "Weight: " << snacks[i].weight << endl;
+        cout << "Calorie: " << snacks[i].calorie << endl;   
+    }
+	cin.get();
+    
+    return 0;
+}
