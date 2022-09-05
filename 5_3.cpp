683a7b923ba05d8f365b477bcdd8b9dcd3b29fe8
Lovedting <110101755+Lovedting@users.noreply.github.com>
2022-09-05 22:28:49 +0800

    Add files via upload

---
+++ 5_3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+int main()
+{
+using namespace std;
+long long i=1;
+long long s=0;
+//initialization
+while(i!=0)
+{
+	cout<<"type in numbers:";
+	cin>>i;
+	s+=i;
+	cout<<"s="<<s<<endl;
+}
+//
+  cin.get();
+  cin.get();
+  return 0;
+}
