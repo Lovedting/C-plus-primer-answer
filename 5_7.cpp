cc88c9e49feeadfabcb1bf73aeca2a237bd561bd
Lovedting <jiuni073@gmail.com>
2022-09-25 22:16:48 +0800

    Update 5_7.cpp

--- 5_7.cpp
+++ 5_7.cpp
@@ -10,8 +10,12 @@ int main()
 		char *make;
 		int year;
 	};
+	
+	
 	int i;
 	car *Car=new car[catalog];
+	
+	
 	for(i=0;i<catalog;i++)
 	{
 		cout<<"Car #"<<i<<":\n"<<"Please enter the make:";
@@ -19,9 +23,12 @@ int main()
 		cout<<"\n"<<"Please enter the year made:";
 		cin>>(Car+i)->year;
 	};
+	
+	
 	cout<<"Here is your collection:"<<"\n";
 	for(i=0;i<catalog;i++)
 		cout<<(Car+i)->year<<" "<<(Car+i)->make<<"\n";
+	
 	delete []Car;
 	cin.get();
 	cin.get();
